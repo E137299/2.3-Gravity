1481ea1b5b53067c53a57f5cfd3ce1cfaa05dc70
David Yin <29613693+E137299@users.noreply.github.com>
2025-09-11 10:14:45 -0500

    Done

--- GravityCalculator.java
+++ GravityCalculator.java
@@ -1,4 +1,8 @@
-class GravityCalculator {
-    // Static method to calculate the final position
-    
+public class GravityCalculator 
+{
+    public static double calculateFinalPosition(double gravity, double initialVelocity, double fallingTime, double initialPosition) 
+    {
+        // Formula: y = 0.5 * g * t^2 + v0 * t + y0
+        return 0.5 * gravity * fallingTime * fallingTime + initialVelocity * fallingTime + initialPosition;
+    }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
-public class Main {
-    public static void main(String[] args) {
-        
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        double test = GravityCalculator.calculateFinalPosition(9.8, 20.1, 10, 45);
+        System.out.println("test "+test);
     }
 }
